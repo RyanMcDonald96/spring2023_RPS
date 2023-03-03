3a0f59cabcf03ea7963639cb890924957a627055
RyanMcDonald96 <126900927+RyanMcDonald96@users.noreply.github.com>
2023-03-03 09:35:16 -0800

    Added more lines

--- rps.py
+++ rps.py
@@ -28,6 +28,14 @@ BLUE = (0, 0, 255)
 
 choices0 = ["rock", "paper", "scissors"]
 
+def draw_text(text, size, color, x, y):
+    font_name = pg.font.match_font('arial')
+    font = pg.font.Font(font_name, size)
+    text_surface = font.render(text, True, color)
+    text_rect = text_surface.get_rect()
+    text_rect.midtop = (x,y)
+    screen.blit(text_surface, text_rect)
+
 def cpu_randomchoice():
     print("computer randomly decides ")
     global cpu_choice
@@ -44,21 +52,23 @@ clock = pg.time.Clock()
 
 rock_image = pg.image.load(os.path.join(game_folder, 'rock.jpg')).convert()
 rock_image_rect = rock_image.get_rect()
-rock_image_rect.x = 50 
+cpu_rock_image_rect = rock_image.get_rect()
 
 scissor_image = pg.image.load(os.path.join(game_folder, 'scissors.png')).convert()
 scissor_image_rect = scissor_image.get_rect()
-scissor_image_rect.x = 300
+cpu_scissor_image_rect = scissor_image.get_rect()
 
 
 paper_image = pg.image.load(os.path.join(game_folder, 'paper.jpg')).convert()
 paper_image_rect = rock_image.get_rect()
-paper_image_rect.x = 500
+cpu_paper_image_rect = paper_image.get_rect()
 
+start_screen = True
 
-running = True
 player_choice = ""
 cpu_choice = ""
+running = True
+
 
 while running:
     clock.tick(FPS)
@@ -66,6 +76,10 @@ while running:
     for event in pg.event.get():
         if event.type == pg.QUIT:
             running = False
+        if event.type == pg.KEYDOWN:
+            if event.key == pg.K_SPACE:
+                print("Let the game begin!")
+                start_screen = False
         if event.type == pg.MOUSEBUTTONUP:
             # 
             print(pg.mouse.get_pos()[0])
@@ -81,7 +95,7 @@ while running:
                 print("rock")
                 player_choice = "rock"
                 cpu_choice = cpu_randomchoice()
-
+                # call a function that gets the cpu choice
             elif scissor_image_rect.collidepoint(mouse_coords):
                 print("scissors")
                 player_choice = "scissors"
@@ -89,7 +103,7 @@ while running:
 
             elif paper_image_rect.collidepoint(mouse_coords):
                 print("paper")
-                user = "paper"
+                player_choice = "paper"
                 cpu_choice = cpu_randomchoice()
             else:
                 print("you didn't click anything")
@@ -98,10 +112,34 @@ while running:
     ########## update ############
     screen.fill(BLACK)
 
-    if player_choice == "":
+    if start_screen:
+        draw_text("Press space to play rock paper scissors", 22, WHITE, WIDTH/2, HEIGHT/10)
+        rock_image_rect.x = 2000
+        paper_image_rect.x = 2000
+        scissor_image_rect.x = 2000
+
+    if not start_screen and player_choice == "":
+        rock_image_rect.x = 50
+        paper_image_rect.x = 350
+        scissor_image_rect.x = 550
+        screen.blit(scissor_image, scissor_image_rect)
+        screen.blit(paper_image, paper_image_rect)
+        screen.blit(rock_image, rock_image_rect)
+
+    if not start_screen and player_choice == "":
+        rock_image_rect.x = 50
+        paper_image_rect.x = 350
+        scissor_image_rect.x = 550
         screen.blit(rock_image, rock_image_rect)
         screen.blit(paper_image, paper_image_rect) 
         screen.blit(scissor_image, scissor_image_rect)
+
+    if player_choice == "rock" and cpu_choice == "rock":
+        cpu_rock_image_rect.x = 500
+        screen.blit(rock_image, rock_image_rect)
+        screen.blit(rock_image, cpu_rock_image_rect)
+        draw_text("You tied!!!", 22, WHITE, WIDTH/2, HEIGHT/10)
+
     
     if player_choice == "rock" and cpu_choice == "scissors":
         screen.blit(rock_image, rock_image_rect)
